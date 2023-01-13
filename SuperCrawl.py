fbeab9e426f23ad4b0483498e7e0458a888b8274
ayazukuri <korscr1027@yahoo.com>
2023-01-13 17:02:08 +0100

    feat: final every-function, user agent set to pc

--- SuperCrawl.py
+++ SuperCrawl.py
@@ -8,6 +8,23 @@ def on_page(func: Callable[[PageContext], Awaitable]):
         return func(lh.ctx)
     return fn
 
+def mark_done(lh: LogicHandle) -> Awaitable:
+    return lh.element_handle.evaluate("node => node.classList.add('sc-handled')")
+
+def scroll_by(x: float, y: float):
+    @on_page
+    def fn(page: PageContext):
+        return page.page.mouse.wheel(x, y)
+    return fn
+
+async def scroll_down_to(lh: LogicHandle):
+    while not await lh.element_handle.is_visible():
+        await lh.ctx.page.mouse.wheel(0, 100)
+
+@on_page
+async def scroll_end(page: PageContext):
+    await page.page.keyboard.down("End")
+    
 class LogicHandle:
     ctx: PageContext
     element_handle: ElementHandle
@@ -24,17 +41,21 @@ class Routine:
     ctx: PageContext
     query: str
     cbs: tuple[Callable[[LogicHandle], Awaitable]]
+    final: Callable[[LogicHandle], Awaitable]
 
-    def __init__(self, ctx: PageContext, query: str, cbs: tuple[Callable[[LogicHandle]], Awaitable]):
+    def __init__(self, ctx: PageContext, query: str, cbs: tuple[Callable[[LogicHandle]], Awaitable], final: Callable[[LogicHandle], Awaitable] | None):
         self.ctx = ctx
         self.query = query
         self.cbs = cbs
+        self.final = final
     
     async def run(self, page: Page) -> None:
         handle = await page.wait_for_selector(self.query)
         lh = LogicHandle(self.ctx, handle)
         for cb in self.cbs:
             await cb(lh)
+        if self.final:
+            await self.final(lh)
 
 class PageContext:
     sc: SuperCrawl
@@ -49,15 +70,18 @@ class PageContext:
         self.running = False
         self._routines = []
     
-    def every(self, query: str, *cbs: Callable[[LogicHandle], Awaitable]) -> None:
-        self._routines.append(Routine(self, query, cbs))
+    def every(self, query: str, *cbs: Callable[[LogicHandle], Awaitable], final=None) -> None:
+        self._routines.append(Routine(self, query, cbs, final=final))
+    
+    def every_unique(self, query: str, *cbs: Callable[[LogicHandle], Awaitable], final=None) -> None:
+        self._routines.append(Routine(self, f"{query}:not(.sc-handled)", cbs + (mark_done,), final=final))
     
     async def _apply_routine(self, routine: Routine) -> None:
         while self.running:
             await routine.run(self.page)
 
     async def _init_loop(self) -> None:
-        context = await self.sc.browser.new_context()
+        context = await self.sc.browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
         self.page = await context.new_page()
         await self.page.goto(self.url)
         self.running = True
@@ -77,5 +101,5 @@ class SuperCrawl:
     
     async def run(self) -> None:
         apw = await async_playwright().start()
-        self.browser = await apw.chromium.launch(headless=False)
+        self.browser = await apw.chromium.launch(headless=True)
         await gather(*map(lambda ctx: ctx._init_loop(), self._ctx))
--- test.py
+++ test.py
@@ -1,19 +1,14 @@
-from SuperCrawl import SuperCrawl, LogicHandle, PageContext, on_page
+from SuperCrawl import SuperCrawl, LogicHandle, PageContext, scroll_end
 from asyncio import run, sleep
 
 async def print_lh(lh: LogicHandle):
     h2 = await lh.element_handle.query_selector("h2")
     if h2:
-        print(await h2.inner_html())
-    await lh.element_handle.evaluate("node => node.remove()")
-
-@on_page
-async def screenshot(ctx: PageContext):
-    page = ctx.page
-    await page.screenshot(path="./img/screenshot.png")
+        t = await h2.inner_html()
+        print(t)
 
 sc = SuperCrawl()
 ctx = sc.ctx("https://9gag.com")
-ctx.every("article", print_lh, screenshot)
+ctx.every_unique("article", print_lh, final=scroll_end)
 
 run(sc.run())
