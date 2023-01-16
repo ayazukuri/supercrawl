0d7593854091afb9c4b53e261f987369d890b114
ayazukuri <korscr1027@yahoo.com>
2023-01-16 21:35:30 +0100

    feat: sub-instances within routines

--- SuperCrawl.py
+++ SuperCrawl.py
@@ -3,34 +3,15 @@ from typing import Callable, Awaitable
 from asyncio import gather
 from playwright.async_api import async_playwright, Browser, ElementHandle, Page
 
-def on_page(func: Callable[[PageContext], Awaitable]):
-    def fn(lh: LogicHandle) -> Awaitable:
-        return func(lh.ctx)
-    return fn
-
 def mark_done(lh: LogicHandle) -> Awaitable:
-    return lh.element_handle.evaluate("node => node.classList.add('sc-handled')")
-
-def scroll_by(x: float, y: float):
-    @on_page
-    def fn(page: PageContext):
-        return page.page.mouse.wheel(x, y)
-    return fn
-
-async def scroll_down_to(lh: LogicHandle):
-    while not await lh.element_handle.is_visible():
-        await lh.ctx.page.mouse.wheel(0, 100)
-
-@on_page
-async def scroll_end(page: PageContext):
-    await page.page.keyboard.down("End")
+    return lh.element_handle.evaluate("node => node.classList.add('supercrawl-handled')")
     
 class LogicHandle:
-    ctx: PageContext
+    sc: SuperCrawl
     element_handle: ElementHandle
 
-    def __init__(self, ctx: PageContext, element_handle: ElementHandle):
-        self.ctx = ctx
+    def __init__(self, sc: SuperCrawl, element_handle: ElementHandle):
+        self.sc = sc
         self.element_handle = element_handle
     
     async def log(self):
@@ -38,68 +19,72 @@ class LogicHandle:
         print(await el.bounding_box())
 
 class Routine:
-    ctx: PageContext
+    sc: SuperCrawl
     query: str
     cbs: tuple[Callable[[LogicHandle], Awaitable]]
     final: Callable[[LogicHandle], Awaitable]
 
-    def __init__(self, ctx: PageContext, query: str, cbs: tuple[Callable[[LogicHandle]], Awaitable], final: Callable[[LogicHandle], Awaitable] | None):
-        self.ctx = ctx
+    def __init__(self, sc: SuperCrawl, query: str, cbs: tuple[Callable[[LogicHandle]], Awaitable], final: Callable[[LogicHandle], Awaitable] | None = None):
+        self.sc = sc
         self.query = query
         self.cbs = cbs
         self.final = final
     
     async def run(self, page: Page) -> None:
         handle = await page.wait_for_selector(self.query)
-        lh = LogicHandle(self.ctx, handle)
+        lh = LogicHandle(self.sc, handle)
         for cb in self.cbs:
             await cb(lh)
         if self.final:
             await self.final(lh)
 
-class PageContext:
-    sc: SuperCrawl
-    page: Page
+class SuperCrawl:
+    browser: Browser | None
     url: str
+    page: Page | None
     running: bool
     _routines: list[Routine]
+    _subs: list[SuperCrawl]
 
-    def __init__(self, sc: SuperCrawl, url: str):
-        self.sc = sc
-        self.url = url
+    def __init__(self, *arg: str, browser: Browser | None = None):
+        if len(arg) > 0:
+            self.url = arg[0]
+        else:
+            self.url = None
+        self.browser = browser
         self.running = False
         self._routines = []
+        self._subs = []
+    
+    def sub(self, *arg: str) -> SuperCrawl:
+        if not self.browser:
+            raise ValueError("uninitialized SuperCrawl instance is unable to create a subinstance")
+        return SuperCrawl(arg[0] if len(arg) > 0 else None, browser=self.browser)
     
     def every(self, query: str, *cbs: Callable[[LogicHandle], Awaitable], final=None) -> None:
         self._routines.append(Routine(self, query, cbs, final=final))
     
     def every_unique(self, query: str, *cbs: Callable[[LogicHandle], Awaitable], final=None) -> None:
-        self._routines.append(Routine(self, f"{query}:not(.sc-handled)", cbs + (mark_done,), final=final))
-    
-    async def _apply_routine(self, routine: Routine) -> None:
-        while self.running:
-            await routine.run(self.page)
+        self.every(f"{query}:not(.supercrawl-handled)", *cbs, mark_done, final=final)
 
-    async def _init_loop(self) -> None:
-        context = await self.sc.browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+    async def _init(self) -> None:
+        if not self.browser:
+            apw = await async_playwright().start()
+            self.browser = await apw.chromium.launch(headless=False)
+        context = await self.browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
         self.page = await context.new_page()
         await self.page.goto(self.url)
         self.running = True
-        await gather(*map(lambda r: self._apply_routine(r), self._routines))
-
-class SuperCrawl:
-    browser: Browser
-    _ctx: list[PageContext]
-
-    def __init__(self):
-        self._ctx = []
     
-    def ctx(self, url: str):
-        ctx = PageContext(self, url)
-        self._ctx.append(ctx)
-        return ctx
+    async def _apply_routine_loop(self, routine: Routine) -> None:
+        # TODO: somehow break loop
+        while self.running:
+            await routine.run(self.page)
     
     async def run(self) -> None:
-        apw = await async_playwright().start()
-        self.browser = await apw.chromium.launch(headless=False)
-        await gather(*map(lambda ctx: ctx._init_loop(), self._ctx))
+        await self._init()
+        await gather(*map(lambda r: r.run(self.page), self._routines))
+
+    async def loop(self) -> None:
+        await self._init()
+        await gather(*map(lambda r: self._apply_routine_loop(r), self._routines))
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from SuperCrawl import SuperCrawl, LogicHandle, PageContext, scroll_end
-from asyncio import run, sleep
+from SuperCrawl import SuperCrawl, LogicHandle
+from asyncio import run
 
 async def print_lh(lh: LogicHandle):
     h2 = await lh.element_handle.query_selector("h2")
@@ -7,8 +7,19 @@ async def print_lh(lh: LogicHandle):
         t = await h2.inner_html()
         print(t)
 
-sc = SuperCrawl()
-ctx = sc.ctx("https://9gag.com")
-ctx.every_unique("article", print_lh, final=scroll_end)
+async def printl(lh: LogicHandle):
+    print("Found a comment!")
 
-run(sc.run())
+async def comm_h(lh: LogicHandle):
+    c = await lh.element_handle.query_selector("a.comment")
+    if not c: return
+    url = await c.get_attribute("href")
+    if not url or url == "javascript:void(0);": return
+    sub = lh.sc.sub(f"https://9gag.com{url}")
+    sub.every_unique("section.comment-list", printl)
+    await sub.run()
+
+sc = SuperCrawl("https://9gag.com")
+sc.every_unique("article", comm_h)
+
+run(sc.loop())
