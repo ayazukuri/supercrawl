da5af0f5f72aaf65740c07eeed496d6fbe21ba71
ayazukuri <korscr1027@yahoo.com>
2023-03-17 15:23:49 +0100

    feat: mongo driver + example code

--- MongoDriver.py
+++ MongoDriver.py
@@ -1,13 +1,106 @@
-from typing import overload
+from __future__ import annotations
+from typing import Any, Generic, TypeVar, Callable
+from abc import ABC as Abstract, abstractmethod as abstract
+from asyncio import create_task, sleep, wait, Task, Future, FIRST_COMPLETED
 from pymongo.mongo_client import MongoClient
+from pymongo.database import Database
+from pymongo.operations import UpdateOne
 
-class MongoDriver:
-    client: MongoClient
+T = TypeVar("T")
 
-    def __init__(self, arg: str | MongoClient):
-        if isinstance(arg, str):
-            self.client = MongoClient(arg)
-        elif isinstance(arg, MongoClient):
-            self.client = arg
+def group(l: list[T], grp_fn: Callable[[T], str]) -> dict[str, list[T]]:
+    d: dict[str, list[T]] = {}
+    for item in l:
+        al = grp_fn(item)
+        if al in d:
+            d[al].append(item)
         else:
-            raise TypeError("arg must be of type str or MongoClient")
+            d[al] = [item]
+    return d
+
+class Serializable(Abstract, Generic[T]):
+    @abstract
+    def id(self) -> Any:
+        pass
+
+    @abstract
+    def serialize(self) -> dict[str, Any]:
+        pass
+
+    @abstract
+    def put(self, k: str, v: T) -> Serializable[T]:
+        pass
+
+    @abstract
+    def get(self, k: str) -> T:
+        pass
+
+    @abstract
+    def destination(self) -> str:
+        pass
+
+class Document(Generic[T], Serializable[T]):
+    _to_coll: str
+    entries: dict[str, T]
+
+    def __init__(self, coll: str):
+        self._to_coll = coll
+        self.entries = {}
+
+    def id(self):
+        return self.entries["_id"]
+
+    def serialize(self):
+        return self.entries
+
+    def put(self, k: str, v: Any):
+        self.entries[k] = v
+        return self
+    
+    def get(self, k: str):
+        return self.entries[k]
+    
+    def destination(self):
+        return self._to_coll
+
+class MongoDriver(Generic[T]):
+    client: MongoClient[dict[str, T]]
+    running: Future[int]
+    _queue: list[Serializable[T]]
+    _db: Database[dict[str, T]]
+    _sync_task: Task[Any]
+
+    def __init__(self, client: MongoClient[dict[str, T]], db: str, sync_delay: int = 60):
+        self.client = client
+        self._queue = []
+        self._db = self.client.get_database(db)
+        self._sync_delay = sync_delay
+
+    def document_factory(self, collection: str) -> Callable[[], Document[T]]:
+        def doc() -> Document[T]:
+            d: Document[T] = Document(collection)
+            self._queue.append(d)
+            return d
+        return doc
+
+    async def _sync(self):
+        while not self.running.done():
+            done, _ = await wait([self.running, sleep(self._sync_delay)], return_when=FIRST_COMPLETED)
+            if 0 in map(lambda task: task.result(), done): return
+            self.sync()
+
+    def sync(self):
+        if len(self._queue) == 0: return
+        for dest, grp in group(self._queue, lambda d: d.destination()).items():
+            self._db.get_collection(dest).bulk_write(list(map(lambda ser: UpdateOne({ "_id": ser.id() }, { "$set": ser.serialize() }, upsert=True), grp)))
+        self._gc()
+
+    def _gc(self):
+        self._queue = []
+
+    async def run(self):
+        self.running = Future()
+        self._sync_task = create_task(self._sync())
+        await self.running
+        await self._sync_task
+        self._gc()
--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 from SuperCrawl import SuperCrawl, LogicHandle
-from playwright.async_api import Page
-from asyncio import run, sleep
+from MongoDriver import MongoDriver
+from pymongo.mongo_client import MongoClient
+from pymongo.operations import UpdateOne
+from asyncio import run, sleep, gather
+from random import random
 
-async def page_waiter(page: Page):
-    await sleep(10)
-    print("short waiter done")
+mc = MongoClient("mongodb://ayasaku:abc123@opengoethe.com:27017")
+handler = MongoDriver(mc, "supercrawl", sync_delay=5)
+fac = handler.document_factory("comments")
 
-async def page_waiter2(page: Page):
-    await sleep(15)
-    print("long waiter done")
+async def page_waiter(lh: LogicHandle):
+    await sleep(3)
 
 async def print_lh(lh: LogicHandle):
     h2 = await lh.element_handle.query_selector("h2")
@@ -17,7 +19,10 @@ async def print_lh(lh: LogicHandle):
         print(t)
 
 async def printl(lh: LogicHandle):
-    print("Found a comment!")
+    t = await lh.element_handle.inner_text()
+    d = fac()
+    d.put("_id", int(random() * 10000))
+    d.put("text", t)
 
 async def comm_h(lh: LogicHandle):
     c = await lh.element_handle.query_selector("a.comment")
@@ -25,10 +30,12 @@ async def comm_h(lh: LogicHandle):
     url = await c.get_attribute("href")
     if not url or url == "javascript:void(0);": return
     sub = lh.sc.sub(f"https://9gag.com{url}")
-    sub.every("section.comment-list", printl)
-    await sub.run(page_waiter2)
+    sub.every("div.comment-list-item__text", printl)
+    await sub.run(page_waiter)
 
 sc = SuperCrawl("https://9gag.com")
 sc.every("article", comm_h)
 
-run(sc.run(page_waiter))
+async def exec_context():
+    await gather(sc.run(page_waiter), handler.run())
+run(exec_context())
